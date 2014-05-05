01fc21a99cda8ebe063b0737d15bee41da3d6032
hqcasanova <develop@hqcasanova.com>
2014-05-05 21:35:57 +0100

    Added support for Bower and Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  'use strict';	
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> v<%= pkg.version %> | (c) 2014 Hector Quintero Casanova | <%= pkg.license %> */\n',
+    // Task configuration.
+    jshint: {
+      src: ['Gruntfile.js', '<%= pkg.name %>.js'],
+      options: {
+	jshintrc: true
+      }     
+    },
+    watch: {
+      files: ['Gruntfile.js', '<%= pkg.name %>.js'],
+      tasks: 'default'
+    },	
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'uglify']);
+};
